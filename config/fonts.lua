602d005a9d0e3e8a209d0583ac9eb4ac2911bb64
Lilanga Gamage <lilanga@live.com>
2024-07-06 16:08:17 +1000

    style: highlight colors and icon styles

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,11 +1,12 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrainsMono Nerd Font'
+local font = 'JetBrainsMono NF'
+local fallbackFont = 'Hack Nerd Font'
 local font_size = platform().is_mac and 12 or 14
 
 return {
-   font = wezterm.font(font),
+   font = wezterm.font_with_fallback({font, fallbackFont}),
    font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
