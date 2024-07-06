cc0634455adf2a668d41a4b681be1d94ee52bb84
Lilanga Gamage <lilanga@live.com>
2024-07-06 16:07:07 +1000

    style: move color definitions to colors.lua

--- utils/colors.lua
+++ utils/colors.lua
@@ -43,5 +43,66 @@ return {
     VERDIGRIS = {
         bg = wezterm.color.parse("#28AFB0"),
         fg = color_default_fg_light
+    },
+    MOCHA = {
+        rosewater = '#f5e0dc',
+        flamingo = '#f2cdcd',
+        pink = '#f5c2e7',
+        mauve = '#cba6f7',
+        red = '#f38ba8',
+        maroon = '#eba0ac',
+        peach = '#fab387',
+        yellow = '#f9e2af',
+        green = '#a6e3a1',
+        teal = '#94e2d5',
+        sky = '#89dceb',
+        sapphire = '#74c7ec',
+        blue = '#89b4fa',
+        lavender = '#b4befe',
+        text = '#cdd6f4',
+        subtext1 = '#bac2de',
+        subtext0 = '#a6adc8',
+        overlay2 = '#9399b2',
+        overlay1 = '#7f849c',
+        overlay0 = '#6c7086',
+        surface2 = '#585b70',
+        surface1 = '#45475a',
+        surface0 = '#313244',
+        surface0_trnsp = 'rgba(49, 50, 68, 0.4)',
+        base = '#1f1f28',
+        mantle = '#181825',
+        crust = '#11111b',
+    },
+    ANSI = {
+        '#0C0C0C', -- black
+        '#C50F1F', -- red
+        '#13A10E', -- green
+        '#C19C00', -- yellow
+        '#0037DA', -- blue
+        '#881798', -- magenta/purple
+        '#3A96DD', -- cyan
+        '#CCCCCC', -- white
+    },
+    BRIGHTS = {
+        '#767676', -- black
+        '#E74856', -- red
+        '#16C60C', -- green
+        '#F9F1A5', -- yellow
+        '#3B78FF', -- blue
+        '#B4009E', -- magenta/purple
+        '#61D6D6', -- cyan
+        '#F2F2F2', -- white
+    },
+    RIGHT_STATUS = {
+        date_fg = '#fab387',
+        date_bg = 'rgba(49, 50, 68, 0.4)',
+        battery_fg = '#f9e2af',
+        battery_bg = 'rgba(49, 50, 68, 0.4)',
+        separator_fg = '#74c7ec',
+        separator_bg = 'rgba(49, 50, 68, 0.4)',
+        domain_fg = '#FBB829',
+        domain_bg = 'rgba(49, 50, 68, 0.4)',
+        weather_fg = '#13A10E',
+        weather_bg = 'rgba(49, 50, 68, 0.4)',
     }
 }
